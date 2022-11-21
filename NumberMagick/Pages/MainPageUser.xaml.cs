6ce2c90bbeb8680590d67371d53f9a1b226ee82a
leeeeeeeeeemon <emilbarish@gmail.com>
2022-11-22 02:48:05 +0300

    Добавьте файлы проекта.

---
+++ NumberMagick/Pages/MainPageUser.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TranslateTest;
+namespace NumberMagick.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPageUser.xaml
+    /// </summary>
+    public partial class MainPageUser : Page
+    {
+        public MainPageUser()
+        {
+            InitializeComponent();
+            format_cb.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btn_generate_Click(object sender, RoutedEventArgs e)
+        {
+            switch(format_cb.SelectedIndex)
+            {
+                case 0:
+                    if (number_tb.Text == "")
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSRandomTrivia();
+                    }
+                    else
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSCurrTrivia(number_tb.Text);
+                    }
+                    break;
+                case 1:
+                    if (number_tb.Text == "")
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSRandomDate();
+                    }
+                    else
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSCurrDate(number_tb.Text);
+                    }
+                    break;
+                case 2:
+                    if (number_tb.Text == "")
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSRandomDate();
+                    }
+                    else
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSCurrDate(number_tb.Text);
+                    }
+                    break;
+                case 3:
+                    if (number_tb.Text == "")
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSRandomMath();
+                    }
+                    else
+                    {
+                        tb_Text.Text = TranslateTest.Generate.GetSCurrMath(number_tb.Text);
+                    }
+                    break;
+            }
+            
+            
+        }
+    }
+}
---
+++ TranslateTest/Generate.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace TranslateTest
+{
+
+    public static class Generate
+    {
+
+        public static string GetSRandomTrivia()
+        {
+            var url = $"http://numbersapi.com/random/trivia";
+
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string tmp = streamReader.ReadToEnd();
+                    return tmp;
+                    //var myDeserializedClass = JsonConvert.DeserializeObject<Root>(tmp);
+
+                    //return myDeserializedClass.desc;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static string GetSRandomYear()
+        {
+            var url = $"http://numbersapi.com/random/year";
+
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string tmp = streamReader.ReadToEnd();
+                    return tmp;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static string GetSRandomDate()
+        {
+            var url = $"http://numbersapi.com/random/date";
+
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string tmp = streamReader.ReadToEnd();
+                    return tmp;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static string GetSRandomMath()
+        {
+            var url = $"http://numbersapi.com/random/math";
+
+            var request = WebRequest.Create(url);
+            var response = request.GetResponse();
+            var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+            if (httpStatusCode == HttpStatusCode.OK)
+            {
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    string tmp = streamReader.ReadToEnd();
+                    return tmp;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static string GetSCurrMath(string Number)
+        {
+            var url = $"http://numbersapi.com/{Number}/math";
+
+            var request = WebRequest.Create(url);
+            try
+            {
+                var response = request.GetResponse();
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode == HttpStatusCode.OK)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string tmp = streamReader.ReadToEnd();
+                        return tmp;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return "Something wrong!";
+            }
+
+        }
+        public static string GetSCurrTrivia(string Number)
+        {
+            var url = $"http://numbersapi.com/{Number}";
+
+            var request = WebRequest.Create(url);
+            try
+            {
+                var response = request.GetResponse();
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode == HttpStatusCode.OK)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string tmp = streamReader.ReadToEnd();
+                        return tmp;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            } catch
+            {
+                return "Something wrong!";
+            }
+            
+        }
+        public static string GetSCurrDate(string Date)
+        {
+            var url = $"http://numbersapi.com/{Date}/date";
+
+            var request = WebRequest.Create(url);
+            try
+            {
+                var response = request.GetResponse();
+                var httpStatusCode = (response as HttpWebResponse).StatusCode;
+
+                if (httpStatusCode == HttpStatusCode.OK)
+                {
+                    using (var streamReader = new StreamReader(response.GetResponseStream()))
+                    {
+                        string tmp = streamReader.ReadToEnd();
+                        return tmp;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return "Something wrong!";
+            }
+
+        }
+    }
+    
+    public class Root
+    {
+        public string desc;
+    }
+}
